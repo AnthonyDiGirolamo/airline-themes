669eb88765e98bea91facd54d667c6d23ca5976b
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-07-06 00:17:36 -0400

    added solarized colors from airline, renamed existing one solarized-alternate

--- airline-themes.el
+++ airline-themes.el
@@ -996,7 +996,7 @@ url: https://github.com/mkaito/base16-emacs"
   (powerline-airline-set-modeline)
 )
 
-(defun airline-theme-solarized-gui ()
+(defun airline-theme-solarized-alternate-gui ()
   ""
   (interactive)
   (let ((normal-outer-foreground  (face-background 'highlight)) (normal-outer-background  (face-foreground 'font-lock-function-name-face))
@@ -1010,6 +1010,34 @@ url: https://github.com/mkaito/base16-emacs"
         (visual-outer-foreground  (face-background 'highlight))          (visual-outer-background  (face-foreground 'font-lock-doc-face))
         (visual-inner-foreground  (face-foreground 'font-lock-doc-face)) (visual-inner-background  (face-background 'fringe))
         (visual-center-foreground (face-foreground 'highlight))          (visual-center-background (face-background 'highlight))
+
+        (replace-outer-foreground (face-background 'highlight)) (replace-outer-background (face-foreground 'error))
+        (emacs-outer-foreground   (face-background 'highlight)) (emacs-outer-background   (face-foreground 'escape-glyph))
+
+        (inactive1-foreground (face-foreground 'fringe)) (inactive1-background (face-foreground 'highlight))
+        (inactive2-foreground (face-foreground 'fringe)) (inactive2-background (face-foreground 'highlight)))
+
+    (powerline-airline-set-face-colors)
+    (powerline-airline-set-cursor-colors)
+    (powerline-airline-set-helm-faces)
+  )
+  (powerline-airline-set-modeline)
+)
+
+(defun airline-theme-solarized-gui ()
+  ""
+  (interactive)
+  (let ((normal-outer-foreground  (face-background 'highlight)) (normal-outer-background  (face-background 'region))
+        (normal-inner-foreground  (face-background 'default))   (normal-inner-background  (face-foreground 'fringe))
+        (normal-center-foreground (face-foreground 'fringe))    (normal-center-background (face-background 'highlight))
+
+        (insert-outer-foreground  (face-background 'highlight)) (insert-outer-background  (face-foreground 'link))
+        (insert-inner-foreground  (face-background 'default))   (insert-inner-background  (face-foreground 'fringe))
+        (insert-center-foreground (face-foreground 'fringe))    (insert-center-background (face-background 'highlight))
+
+        (visual-outer-foreground  (face-background 'highlight)) (visual-outer-background  (face-background 'isearch))
+        (visual-inner-foreground  (face-background 'default))   (visual-inner-background  (face-foreground 'fringe))
+        (visual-center-foreground (face-foreground 'fringe))    (visual-center-background (face-background 'highlight))
 
         (replace-outer-foreground (face-background 'highlight)) (replace-outer-background (face-foreground 'error))
         (emacs-outer-foreground   (face-background 'highlight)) (emacs-outer-background   (face-foreground 'escape-glyph))
