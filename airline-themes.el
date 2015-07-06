fb4ac7cd3e61cf55462edf48131bcb1da7a3fee1
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-07-05 23:00:27 -0400

    added solarized gui colors

--- airline-themes.el
+++ airline-themes.el
@@ -967,6 +967,34 @@ url: https://github.com/mkaito/base16-emacs"
   (powerline-airline-set-modeline)
 )
 
+(defun airline-theme-solarized-dark-gui ()
+  ""
+  (interactive)
+  (let ((normal-outer-foreground  (face-background 'highlight)) (normal-outer-background  (face-foreground 'font-lock-function-name-face))
+        (normal-inner-foreground  (face-foreground 'fringe))    (normal-inner-background  (face-background 'fringe))
+        (normal-center-foreground (face-foreground 'highlight)) (normal-center-background (face-background 'highlight))
+
+        (insert-outer-foreground  (face-background 'highlight)) (insert-outer-background  (face-foreground 'success))
+        (insert-inner-foreground  (face-foreground 'success))   (insert-inner-background  (face-background 'fringe))
+        (insert-center-foreground (face-foreground 'highlight)) (insert-center-background (face-background 'highlight))
+
+        (visual-outer-foreground  (face-background 'highlight))          (visual-outer-background  (face-foreground 'font-lock-doc-face))
+        (visual-inner-foreground  (face-foreground 'font-lock-doc-face)) (visual-inner-background  (face-background 'fringe))
+        (visual-center-foreground (face-foreground 'highlight))          (visual-center-background (face-background 'highlight))
+
+        (replace-outer-foreground (face-background 'highlight)) (replace-outer-background (face-foreground 'error))
+        (emacs-outer-foreground   (face-background 'highlight)) (emacs-outer-background   (face-foreground 'escape-glyph))
+
+        (inactive1-foreground (face-foreground 'fringe)) (inactive1-background (face-foreground 'highlight))
+        (inactive2-foreground (face-foreground 'fringe)) (inactive2-background (face-foreground 'highlight)))
+
+    (powerline-airline-set-face-colors)
+    (powerline-airline-set-cursor-colors)
+    (powerline-airline-set-helm-faces)
+  )
+  (powerline-airline-set-modeline)
+)
+
 ;; (defun airline-theme- ()
 ;;   ""
 ;;   (interactive)
