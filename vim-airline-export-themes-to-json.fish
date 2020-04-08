f190803bda6295f1562fe80c6834a8ba6ee2b025
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-04-08 09:45:13 -0700

    Fix export script

--- vim-airline-export-themes-to-json.fish
+++ vim-airline-export-themes-to-json.fish
@@ -1,12 +1,18 @@
 #!/usr/bin/env fish
-set theme-dir ~/.vim/bundle/vim-airline-themes/autoload/airline/themes
-set outfile vim-airline-theme-palettes.json
-echo '{' > $outfile
+set THEMEDIR ~/.vim/bundle/vim-airline-themes/autoload/airline/themes
+set OUTFILE (pwd)/vim-airline-theme-palettes.json
+cd $THEMEDIR
+set THEME_NAMES (ls *.vim | sort)
 
-cd $themedir
-for f in (ls *.vim)
-  set cs (basename -s .vim $f)
-  vim -c "let g:airline_theme='$cs' | e $outfile | call append('\$', '\"$cs\":') | call append('\$', json_encode(airline#themes#$cs#palette)) | call append('\$', ',')| wq"
+echo '{' > $OUTFILE
+
+for f in $THEME_NAMES
+  set CS (basename -s .vim $f)
+  vim -c "AirlineTheme $CS | e $OUTFILE | call append('\$', '\"$CS\":') | call append('\$', json_encode(airline#themes#$CS#palette)) | wq"
+  # if not last theme
+  if not string match -q $f $THEME_NAMES[-1]
+    echo ',' >> $OUTFILE
+  end
 end
 
-echo '}' >> $outfile
+echo '}' >> $OUTFILE
