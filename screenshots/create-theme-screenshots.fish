c56c1aaaecc64c29854675c694678c11a664a9bf
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2020-04-08 15:17:21 -0700

    Add theme images

--- screenshots/create-theme-screenshots.fish
+++ screenshots/create-theme-screenshots.fish
@@ -6,13 +6,15 @@ rm -f theme-*.png
 for f in (ls 2*.png | sort)
     set outfile (basename -s .png $f)-cropped.png
     set filegroup $filegroup $outfile
+    echo $outfile
 
-    if math "$index % 4 == 0" 1>/dev/null
-        convert -gravity South -crop x92+0+0 $f $outfile
+    if test 0 -eq (math "$index % 4") 1>/dev/null
+        convert -gravity South -crop x89+0+0 $f $outfile
         montage $filegroup -mode concatenate -tile 1x4 theme-(printf "%03d" (math $index/4)).png
         set -e filegroup
+        echo
     else
-        convert -gravity South -crop x46+0+46 $f $outfile
+        convert -gravity South -crop x44+0+44 $f $outfile
     end
 
     set index (math $index+1)
